f133936104b69cfa411be05cfd186f2606ac1a4a
yogesh5454 <yogesh111sapkota@gmail.com>
2025-04-14 14:19:04 +0545

    update

--- src/Simproject.jsx
+++ src/Simproject.jsx
@@ -1,12 +1,18 @@
 import Accordion from "./simprojects/Accordion/Accordion";
 import Randomcolor from "./simprojects/Randomcolor/Randomcolor";
 import Starrating from "./simprojects/Starrating/starrating";
+import ImageSlider from "./simprojects/Imageslider/Imageslider";
 function Simproject() {
   return (
     <>
       <Accordion />
       <Randomcolor />
       <Starrating />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
---
+++ src/simprojects/Imageslider/Imageslider.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+const ImageSlider = ({ url, limit }) => {
+  const [image, setimage] = useState([]);
+  const [currentslide, setcurrentslide] = useState();
+
+  useEffect(() => {}, [url]);
+  return (
+    <>
+      <div>hhhhh</div>
+    </>
+  );
+};
+
+export default ImageSlider;
