06bd18a961ba10683ef68d27a15de41139141ce4
yogesh5454 <yogesh111sapkota@gmail.com>
2025-04-16 11:38:56 +0545

    update

--- src/simprojects/Imageslider/Imageslider.jsx
+++ src/simprojects/Imageslider/Imageslider.jsx
@@ -1,13 +1,63 @@
 import React, { useEffect, useState } from "react";
-
-const ImageSlider = ({ url, limit }) => {
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./image.css";
+const ImageSlider = ({ url, page = 1, limit = 5 }) => {
   const [image, setimage] = useState([]);
-  const [currentslide, setcurrentslide] = useState();
+  const [currentslide, setcurrentslide] = useState(0);
+  const [errormsg, seterrormsg] = useState(null);
+  const [loading, setloading] = useState(false);
+  async function fetchimages(geturl) {
+    try {
+      setloading(true);
+      const response = await fetch(`${geturl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
-  useEffect(() => {}, [url]);
+      if (data) {
+        setimage(data);
+        setloading(false);
+      }
+    } catch (e) {
+      seterrormsg(e.message);
+      setloading(false);
+    }
+  }
+  useEffect(() => {
+    if (url !== "") fetchimages(url);
+  }, [url]);
+  console.log(image);
+
+  if (loading) {
+    return <div>Loading data</div>;
+  }
+  if (errormsg !== null) {
+    return <div>Error occured{errormsg}</div>;
+  }
+
   return (
     <>
-      <div>hhhhh</div>
+      <style>{}</style>
+      <div className="container ">
+        <BsArrowLeftCircleFill className="arrow arrow-left" />
+        {image && image.length
+          ? image.map((imageitem) => (
+              <img
+                key={imageitem.id}
+                alt={imageitem.download_url}
+                src={imageitem.download_url}
+                className="current-image"
+              />
+            ))
+          : null}
+
+        <BsArrowRightCircleFill className="arrow arrow-right" />
+        <span className="circle-indicators">
+          {image && image.length
+            ? image.map((_, index) => (
+                <button key={index} className="current-indicator"></button>
+              ))
+            : null}
+        </span>
+      </div>
     </>
   );
 };
